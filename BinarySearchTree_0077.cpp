49a01a7f7929c3cc27311bc56c47c686f8559f58
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2024-06-14 14:43:36 +0700

    ini berfungsi untuk mencari node saat ini dari node yg ditentukan saat ini dari node

--- BinarySearchTree_0077.cpp
+++ BinarySearchTree_0077.cpp
@@ -36,9 +36,38 @@ public:
         newNode->rightchild = NULL; //Make the right child of the new node point to NULL
 
         Node* parent = NULL;
-        Node currentNode = NULL;
-        search(element, parent, currentNode); //Locate the node with will be the parent of the node to be insereted 
-        
+        Node* currentNode = NULL;
+        search(element, parent,currentNode); //Locate the node with will be the parent of the node to be insereted 
+
+        if (parent == NULL) //if the parent is NULL (Tree is empty)
+        {
+            ROOT = newNode; // Mark the new node as ROOT
+            return; // Exit
+        }
+
+        if (element < parent->info) //if the value in the data field of the new node is less than that of the parent
+        {
+            parent->leftchild = newNode; // Make the left child of the parent point to the new node
+        }
+        else if (element > parent->info) // if the value in the data field of the new node is greater than that of the parent
+        {
+            parent ->rightchild = newNode; //Make the right child of the parent point to the new node
+        }
     }
-   
-}
+
+    void search(string element, Node*& parent, Node*& currentNode)
+    {
+        //This function searches the currentNode of the specified Node as well as the current Node of its parent
+        currentNode = ROOT;
+        parent = NULL;
+        while ((currentNode  != NULL) && (currentNode->info != element))
+        {
+            parent = currentNode;
+            if (element < currentNode->info)
+                currentNode = currentNode->leftchild;
+            else
+                currentNode->rightchild;
+        }
+    }
+
+    
