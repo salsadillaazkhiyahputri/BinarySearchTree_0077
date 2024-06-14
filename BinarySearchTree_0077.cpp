bcee70f30e923685826e0a71c29f860669395802
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2024-06-14 14:44:32 +0700

    fungsi ini untuk membatalkan pesanan pada node* ptr

--- BinarySearchTree_0077.cpp
+++ BinarySearchTree_0077.cpp
@@ -70,4 +70,20 @@ public:
         }
     }
 
+    void inorder(Node* ptr)
+    {
+        if (ROOT == NULL)
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+        if (ptr != NULL)
+        {
+            
+            inorder(ptr->leftchild);
+            cout << ptr ->info << " ";
+            inorder(ptr->rightchild);
+        }
+    }
+
     
